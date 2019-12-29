14e784b6c8eb96c2325bc2d16be1f3fbe88a0690
srabh <srabh@amazon.com>
2019-12-29 21:00:14 +0530

    fixed image path

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {Provider} from 'react-redux';
 import InlineSVG from 'svg-inline-react';
-import  * as logo from './images/logo.svg';
+import logo from './images/logo.svg';
 import './css/App.css';
 import PostForm from './components/PostForm';
 import Posts from './components/Posts';
